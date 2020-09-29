f773cbb337c4dc963a6ad160280a23442b25ed44
frankx13 <frankx13580@gmail.com>
2020-09-30 01:25:52 +0200

    - Removed Network Scanner functionality - Removed 2 buttons that became useless since the Net Info functionality - Added custom button shape

--- app/src/main/java/com/studio/neopanda/easynetfinder/MainActivity.kt
+++ app/src/main/java/com/studio/neopanda/easynetfinder/MainActivity.kt
@@ -1,23 +1,29 @@
 package com.studio.neopanda.easynetfinder
 
+import android.annotation.SuppressLint
 import android.annotation.TargetApi
 import android.content.Context
-import android.net.*
+import android.net.ConnectivityManager
+import android.net.wifi.WifiManager
 import android.os.Build
 import android.os.Bundle
 import android.os.StrictMode
-import android.util.Log
+import android.text.format.Formatter.formatIpAddress
 import android.view.View
 import android.widget.Toast
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.net.Inet4Address
-import java.net.InetAddress
+import java.net.NetworkInterface.getNetworkInterfaces
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
+    private var typeConnection: Int = 0
+    private var currentIPv4: String = ""
+
+    @SuppressLint("SetTextI18n")
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,100 +33,92 @@ class MainActivity : AppCompatActivity() {
         val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
         StrictMode.setThreadPolicy(policy)
 
-        //SHOW IF WE ARE CONNECTED TO NETWORK
-        test_connectivity_btn.setOnClickListener {
-            val cm = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-            val activeNetwork = cm.activeNetworkInfo
-            if (activeNetwork != null) {
-                // connected to the internet
-                if (activeNetwork.type == ConnectivityManager.TYPE_WIFI) {
-                    Toast.makeText(this, "You're connected to a wifi network !", Toast.LENGTH_LONG)
-                        .show()
-                } else if (activeNetwork.type == ConnectivityManager.TYPE_MOBILE) {
-                    Toast.makeText(
-                        this,
-                        "You're connected to a mobile network !",
-                        Toast.LENGTH_LONG
-                    ).show()
-                }
-            } else {
-                Toast.makeText(this, "You're not connected to any network !", Toast.LENGTH_LONG)
-                    .show()
-            }
-        }
-
-        //GET ALL NETWORKS
-        show_networks_btn.setOnClickListener {
-            getAvailableNetwoks(this)
-        }
-
-        show_IP_localhost_btn.setOnClickListener {
+        show_net_inf_btn.setOnClickListener {
+            checkTypeConnection()
             getLocalHostIp()
+            getLoopbackAddress()
+            if (typeConnection == 1) {
+                currentIPv4 = getWifiIPAddress()
+                ipv4_current_result.text = getString(R.string.ipv4_current_search) + currentIPv4
+            } else if (typeConnection == 2) {
+                currentIPv4 = getMobileIPAddress()
+                ipv4_current_result.text = "Current IP : $currentIPv4"
+            }
+
+            ipv4_current_result.visibility = View.VISIBLE
         }
 
-        //TODO: GET ACTIVE NETWORK & INFOS
+        //TODO: GET ACTIVE NETWORK & INFOS OK
         //TODO: VPN CASE
-        //TODO: CREATE SHAPES & THEME FOR THE UI
+        //TODO: CREATE SHAPES FOR THE UI
         //TODO: GET DHCP INFOS
         //TODO: PING FUNCTIONNALITY
         //TODO: TRACERT FUNCTIONNALITY
     }
 
-    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
-    fun getAvailableNetwoks(context: Context): Map<String, Network> {
-        val connManager =
-            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-
-        val setNetworkName = HashMap<String, Network>()
-        if (null != connManager) {
-
-            // Set MOBILE network for checking is it existing
-            activateMobileNetwork(context, connManager)
-
-            val networks = connManager.allNetworks
-            if (networks != null) {
-                var nwInfo: NetworkInfo?
-                for (nw in networks) {
-                    nwInfo = connManager.getNetworkInfo(nw)
-                    setNetworkName[nwInfo!!.typeName] = nw
-                }
+    fun checkTypeConnection(): Int {
+        //SHOW IF WE ARE CONNECTED TO NETWORK
+        val cm = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork = cm.activeNetworkInfo
+        if (activeNetwork != null) {
+            // connected to the internet
+            if (activeNetwork.type == ConnectivityManager.TYPE_WIFI) {
+                typeConnection = 1
+                Toast.makeText(this, "You're connected to a wifi network !", Toast.LENGTH_LONG)
+                    .show()
+            } else if (activeNetwork.type == ConnectivityManager.TYPE_MOBILE) {
+                typeConnection = 2
+                Toast.makeText(
+                    this,
+                    "You're connected to a mobile network !",
+                    Toast.LENGTH_LONG
+                ).show()
             }
+        } else {
+            typeConnection = 3
+            Toast.makeText(this, "You're not connected to any network !", Toast.LENGTH_LONG)
+                .show()
         }
-
-        Log.e("gorteijigire", setNetworkName.toString())
-        Toast.makeText(
-            context,
-            "list of networks : " + setNetworkName.toString(),
-            Toast.LENGTH_LONG
-        ).show()
-
-        return setNetworkName
+        return typeConnection
     }
 
-    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
-    fun activateMobileNetwork(context: Context, connManager: ConnectivityManager) {
-        val builder = NetworkRequest.Builder()
-        builder.addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
-        builder.addTransportType(NetworkCapabilities.TRANSPORT_CELLULAR)
-
-        val networkRequest = builder.build()
-        connManager.requestNetwork(networkRequest, object : ConnectivityManager.NetworkCallback() {
-            override fun onAvailable(network: Network) {
-                super.onAvailable(network)
-                Toast.makeText(context, "MOBILE connect", Toast.LENGTH_SHORT).show()
-            }
-        })
-    }
-
+    @SuppressLint("SetTextI18n")
     fun getLocalHostIp() {
         val hostIPv4 = Inet4Address.getLocalHost().hostAddress
-        ipv4_localhost_result.text = hostIPv4
+        ipv4_localhost_result.text = getString(R.string.ipv4_localhost_search) + hostIPv4
         ipv4_localhost_result.visibility = View.VISIBLE
     }
 
-    fun getCurrentIPv4(){
-
+    @SuppressLint("SetTextI18n")
+    fun getLoopbackAddress() {
+        val loopbackIPv4 = Inet4Address.getLoopbackAddress().address
+        ipv4_loopback_result.text =
+            getString(R.string.ipv4_looback_search) + loopbackIPv4.toString()
+        ipv4_loopback_result.visibility = View.VISIBLE
     }
 
+    fun getWifiIPAddress(): String {
+        val wifiMgr = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        val wifiInfo = wifiMgr.connectionInfo
+        val ip = wifiInfo.ipAddress
+        return formatIpAddress(ip)
+    }
+
+    fun getMobileIPAddress(): String {
+        try {
+            val interfaces = Collections.list(getNetworkInterfaces())
+            for (intf in interfaces) {
+                val addrs = Collections.list(intf.inetAddresses)
+                for (addr in addrs) {
+                    if (!addr.isLoopbackAddress) {
+                        return addr.hostAddress
+                    }
+                }
+            }
+        } catch (ex: Exception) {
+        }
+        // for now eat exceptions
+        return "No way!"
+    }
 
 }
